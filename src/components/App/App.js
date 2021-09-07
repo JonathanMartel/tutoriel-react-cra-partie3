fd151db46807cdacb3d3fa8ce3f96676c3e11e8c
Jonathan Martel <jonathmartel@gmail.com>
2021-09-07 11:32:34 -0400

    Faire remonter l'état d'un composant

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,12 +5,21 @@ import Entete from '../Entete/Entete';
 import Compteur from '../Compteur/Compteur';
 
 export default class App extends React.Component {
-  
+  constructor(){
+    super();
+    this.ajouterAuCompte = this.ajouterAuCompte.bind(this);
+    this.state = {valeur: 0};
+
+  }
+  ajouterAuCompte(){
+    console.log("compteur appelé")
+    this.setState((state) => ({valeur : state.valeur +1}));
+  }
   render(){
     return (
       <div>
         <Entete titre="Site react" />
-        <Compteur valeurInitiale="0" />      
+        <Compteur surChangementCompte={this.ajouterAuCompte} valeur={this.state.valeur} />      
       </div>
     );
   }
--- src/components/Compteur/Compteur.js
+++ src/components/Compteur/Compteur.js
@@ -5,17 +5,18 @@ import './Compteur.css';
 export default class Compteur extends React.Component {
     constructor(props){
         super(props);
-        const nValeur = this.props.valeurInitiale || 0;
-        this.state = {valeur : parseInt(nValeur)};
+        //const nValeur = this.props.valeurInitiale || 0;
+        //this.state = {valeur : parseInt(nValeur)};
 
-        this.ajouterAuCompte = this.ajouterAuCompte.bind(this);
+        //this.ajouterAuCompte = this.ajouterAuCompte.bind(this);
     }
 
-    ajouterAuCompte(){
-        console.log("ajouté");
-        console.log(this);
-        this.setState((state) => ({valeur : state.valeur +1}));
-            
+    //ajouterAuCompte(){
+    //    console.log("ajouté");
+    //    console.log(this);
+    //    this.setState((state) => ({valeur : state.valeur +1}));
+    //    this.props.surChangementCompte(this.state.valeur);
+
         /*this.setState(function(state, props){
             return {valeur : state.valeur +1};
         });*/
@@ -24,13 +25,13 @@ export default class Compteur extends React.Component {
             objet.valeur = state.valeur + 1;
             return objet;
         });*/
-    }
+    //}
 
     render(){
         return (
             <div>
-                <p>Compte jusqu'à maintenant : <span>{this.state.valeur}</span></p>
-                <button onClick={this.ajouterAuCompte}>Cliquez pour ajouter</button>
+                <p>Compte jusqu'à maintenant : <span>{this.props.valeur}</span></p>
+                <button onClick={this.props.surChangementCompte}>Cliquez pour ajouter</button>
             </div>
         );
     }
