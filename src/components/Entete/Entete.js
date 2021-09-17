c2d6c69d9eead5fdc92d76d2b1f9771552ac1875
Jonathan Martel <jonathmartel@gmail.com>
2021-09-16 20:41:51 -0400

    correction du navlink (exact)

--- src/components/Entete/Entete.js
+++ src/components/Entete/Entete.js
@@ -10,16 +10,16 @@ export default class Entete extends React.Component {
   render(){ 
     return (    //JSX
       <header className="entete">
-          <NavLink to="/"><h1>{this.props.titre || "Titre du site"}</h1></NavLink>
+          <NavLink exact to="/"><h1>{this.props.titre || "Titre du site"}</h1></NavLink>
           <nav className="menu">
               <BoutonNav lien="/" label="BtnNav Accueil" />
               <Link to="/">Accueil</Link>
               <BoutonNav lien="/compteur" label="BtnNav compteur" />
               <Link to="/compteur">Compteur</Link>
-              <NavLink to="/compteur">Compteur (navLink)</NavLink>
+              <NavLink exact to="/compteur">Compteur (navLink)</NavLink>
               <BoutonNav lien="/information" label="BtnNav Info" />
               <Link to="/information">Information</Link>
-              <NavLink to="/information">Information(navLink)</NavLink>
+              <NavLink exact to="/information">Information(navLink)</NavLink>
           </nav>
       </header>  
     );
